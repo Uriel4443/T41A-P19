bc48ef7128ac9fcb8709a5ff3cf74d18c51e3be9
Uriel4443 <182829@upslp.edu.mx>
2025-11-24 04:17:52 -0600

    Update 01_create_table.sql

--- 01_create_table.sql
+++ 01_create_table.sql
@@ -1,3 +1,4 @@
 https://onecompiler.com/postgresql/44568uth9
 https://onecompiler.com/postgresql/4456kzumw
 https://onecompiler.com/postgresql/4456wwv2n
+https://onecompiler.com/postgresql/445kth6du
